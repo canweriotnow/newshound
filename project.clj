da62a030a7eee0546bb36a43228a3117f4bf5e50
Jason Lewis <jlewis73@jhu.edu>
2012-11-01 04:56:00 -0400

    Create basic namespaces

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject newshound "0.0.1-SNAPSHOT"
-  :description "Ace Reporting"
-  :url "http://example.com/FIXME"
+  :description "Newshound: Ace Reporting"
+  :url "https://github.com/canweriotnow/newshound"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojars.ghoseb/cron4j "2.2.1"]
                  [com.draines/postal "1.8.0"]
                  [hiccup "1.0.1"]
+                 [org.apache.poi/poi "3.8"]
                  [korma "0.3.0-beta7"]
                  ;; Add the appropriate JDBC drivers for databases here.
                  ]
---
+++ src/newshound/templates/excel.clj
@@ -0,0 +1,1 @@
+(ns newshound.templates.excel)
